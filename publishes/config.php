0e7075e1960b07f2694bc4c0a59b722e71e1197e
endi <endi1982@gmail.com>
2016-02-23 18:17:49 +0200

    Make package configurable

--- publishes/config.php
+++ publishes/config.php
@@ -1,1 +1,14 @@
 <?php
+
+return [
+    'template' => 'full', // available: full, two-columns
+    'map' => [
+        'key' => '',
+        'center' => [
+            'lat' => 47.0104529,
+            'lng' => 28.8638103,
+        ],
+        'zoom' => 8,
+        'fitBounds' => true,
+    ],
+];
